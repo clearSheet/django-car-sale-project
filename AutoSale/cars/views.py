63b3a9473530c161f2f033c241dd6f237d571b40
wooziee <wooozieeeee@gmail.com>
2023-01-17 23:06:32 +0400

    add models and brands for add new order

--- AutoSale/cars/views.py
+++ AutoSale/cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Card_Cars
+from .models import Model_and_Brand_Cars
 from .  func import get_good_price
 
 
@@ -7,6 +8,7 @@ def cars_home(request):
     cars = Card_Cars.objects.all()
     for car in cars:
         car.price = get_good_price(car.price) + ' ₽'
+
     return render(request, 'cars/cars_home.html', {'cars': cars})
 
     # cars = Card_Cars.objects.order_by('price') # Сортировка по цене
@@ -15,4 +17,10 @@ def cars_home(request):
 
 
 def add_new_car(request):
-    return render(request, 'cars/add_new_car.html')
+    brands_and_models = Model_and_Brand_Cars.objects.all()
+
+    return render(
+                    request,
+                    'cars/add_new_car.html',
+                    {'brands_and_models': brands_and_models}
+                  )
